bb02e6f57571032f64327097d385ca24c42934a3
Hector M <moralh2@gmail.com>
2018-11-10 15:08:14 -0600

    added collpasible div for gifs to separate batches from each pagination

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,11 +4,8 @@ $(document).ready(function () {
 
 var topics = ["happy", "sad", "angry", "overwhelmed", "scared", "drunk", "tired", "stressed", "confused", "overworked", "lonely", "ashamed", "excited", "crazy", "amused", "impressed", "shocked", "sick", "annoyed", "pensive", "proud"]
 var pagination = 0
-console.log("init------\n"+pagination+"\n------\n")
 
 function callAPI(keyword) {
-    console.log("api_call------\n"+pagination+"\n------\n")
-
     var api_key = "Xn6YqGd64F2yEYT65oUuwsZkVPMF0ocO"
     // var api_key = "dc6zaTOxFJmzC"
     var offset = pagination * 10
@@ -25,38 +22,28 @@ function callAPI(keyword) {
 
 function loadGifButtons() {
     var btnDiv = $("#gif-buttons")
-    pagination = 1
+    pagination = 0
     btnDiv.empty()
     for (var i in topics) {
         var btn = $("<button>").addClass("btn btn-outline-danger mr-2 mb-2").attr("data-emotion", topics[i]).text(topics[i])
         btn.on("click", function () {
             $("#gif-me").empty();
             var emotion = $(this).attr("data-emotion")
+            pagination = 0
             callAPI(emotion)
                             
             var loadMore = $("#load-more")
             loadMore.empty()
-            var btn2 = $("<button>").addClass("btn btn-danger mr-2").text("load 10 more")
+            var btn2 = $("<button>").addClass("btn btn-danger m-4").text("load 10 more")
             btn2.on("click", function() {
-                console.log("from-loadmore------\n"+pagination+"\n------\n")
-
                 pagination++
                 callAPI(emotion)
             })
             $("#load-more").append(btn2)
-            // set pagination to 1
-            // add new button (+) before gifs (10+)
-            // add event for click
-            // call fnc to request 10 more
-            // --- change fncs
-            // add small text under btns (explain the +)
-
         })
         btnDiv.append(btn)
     }
-
 }
-
 
 $("#add-new-btn").on("click", function (event) {
     event.preventDefault();
@@ -70,6 +57,9 @@ $("#add-new-btn").on("click", function (event) {
 })
 
 function handleResponse(results) {
+    var wrapperWrapper = $("<div>").addClass("row")
+    var wrapper = $("<div>").addClass("collapse show multi-collapse col-md-12").attr('id', 'gifCollapseDiv'+pagination)
+
     for (var i = 0; i < results.length; i++) {
         data = {
             'title': results[i].title,
@@ -78,8 +68,6 @@ function handleResponse(results) {
             'imported': results[i].import_datetime.substring(0, 10),
             'trending': results[i].trending_datetime.substring(0, 10),
             'source': results[i].source_tld
-            // 'eurl': results[i].url,
-            // 'slug': results[i].slug
         }
         var dataRow = $("<div>").addClass("row mt-4 bgo")
         var dataDiv = createDataDiv(data)
@@ -89,7 +77,6 @@ function handleResponse(results) {
         emotionImage.attr("data-still", results[i].images.fixed_height_still.url);
         emotionImage.attr("data-animate", results[i].images.fixed_height.url);
         emotionImage.attr("data-state", 'still');
-
         emotionImage.on("click", function () {
             var state = $(this).attr("data-state");
             if (state === "still") {
@@ -103,8 +90,33 @@ function handleResponse(results) {
         gifDiv.prepend(emotionImage);
         dataRow.append(gifDiv);
         dataRow.append(dataDiv);
-        $("#gif-me").prepend(dataRow);
+        wrapper.append(dataRow)
     }
+    wrapperWrapper.append(wrapper)
+
+    var collapsaBtn = $('<a>').addClass("btn btn-primary btn-sm mb-2")
+    collapsaBtn.attr("data-toggle", "collapse")
+    collapsaBtn.attr("href", "#gifCollapseDiv"+pagination)
+    collapsaBtn.attr("role", "button")
+    collapsaBtn.attr("aria-expanded", "false")
+    var indeces = [ String((pagination)*10+1), String((pagination+1)*10) ]
+    console.log(indeces[0].length)
+    console.log(indeces[1].length)
+    if (indeces[0].length < indeces[1].length) { 
+        indeces[0] = "0" + indeces[0]
+    }
+    collapsaBtn.text("  "+indeces[0]+"-"+indeces[1])
+    var collapseIcon1 = $("<i>").addClass("fas fa-caret-up")
+    var collapseIcon2 = $("<i>").addClass("fas fa-caret-down")
+    collapsaBtn.prepend(collapseIcon2).prepend(collapseIcon1)
+
+    // collapsaBtn.append(collapseIcons)
+    // collapsaBtn.text("1-10")
+    var collDiv = $('<div>').addClass("col-md-12 text-center")
+    collDiv.append(collapsaBtn)
+
+    wrapperWrapper.prepend(collDiv)
+    $("#gif-me").prepend(wrapperWrapper);
 }
 
 function createDataDiv(data) {
