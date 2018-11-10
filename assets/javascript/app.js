0b6130bc532d436f45929a007e1f2529a0ba31a0
Hector M <moralh2@gmail.com>
2018-11-10 04:07:36 -0600

    added new styling to fnc that creates the gif content, added js fnc to create btns from topic list

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,31 @@
-$("button").on("click", function () {
-    var emotion = $(this).attr("data-emotion");
-    var api_key = "Xn6YqGd64F2yEYT65oUuwsZkVPMF0ocO"
-    // var api_key = "dc6zaTOxFJmzC"
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        emotion + "&api_key=" + api_key + "&limit=10";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-        handleResponse(response.data)
-    })
+$(document).ready(function() { 
+    loadGifButtons()
 })
+
+var topics = ["happy", "sad", "angry", "scared", "drunk", "tired", "confused", "overwhelmed", "overworked", "stressed", "lonely", "ashamed", "excited", "crazy", "amused", "impressed", "shocked", "sick"]
+
+function loadGifButtons() {
+    var btnDiv = $("#gif-buttons")
+    btnDiv.empty()
+    for (var i in topics) {
+        var btn = $("<button>").addClass("btn btn-outline-danger mr-2 mb-2").attr("data-emotion", topics[i]).text(topics[i])
+        btn.on("click", function () {
+            var emotion = $(this).attr("data-emotion")
+            var api_key = "Xn6YqGd64F2yEYT65oUuwsZkVPMF0ocO"
+            // var api_key = "dc6zaTOxFJmzC"
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+                emotion + "&api_key=" + api_key + "&limit=10";
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
+                handleResponse(response.data)
+            })
+        })
+        btnDiv.append(btn)
+    }
+}
 
 function handleResponse(results) {
     $("#gif-me").empty();
@@ -25,12 +39,9 @@ function handleResponse(results) {
             'source': results[i].source_tld
         }
         var dataRow = $("<div>").addClass("row mt-4")
-        // dataRow.addClass("row mt-4")
         var dataDiv = createDataDiv(data)
         var gifDiv = $("<div>").addClass("col-lg-3 offset-lg-1 col-md-12 mb-3")
-        // gifDiv.addClass("col-lg-3 offset-lg-1 col-md-12 mb-3")
         var emotionImage = $("<img>").addClass("gif img-fluid rounded mx-auto d-block")
-        // emotionImage.addClass("gif img-fluid rounded mx-auto d-block")
         emotionImage.attr("src", results[i].images.fixed_height_still.url);
         emotionImage.attr("data-still", results[i].images.fixed_height_still.url);
         emotionImage.attr("data-animate", results[i].images.fixed_height.url);
@@ -55,25 +66,18 @@ function handleResponse(results) {
 
 function createDataDiv(data) {
     var dataDiv = $("<div>").addClass("col-lg-7 col-md-12")
-    // dataDiv.addClass("col-lg-7 col-md-12")
     var tableDiv = $("<div>").addClass("table-responsive")
-    // dataDiv.addClass("table-responsive")
     var table = $("<table>").addClass("table table-sm table-dark table-bordered table-striped")
-    // table.addClass("table table-sm table-dark table-bordered table-striped")
     var tbody = $("<tbody>")
 
     for (var key in data) {
         var row = $("<tr>")
         var head = $("<th>").addClass("text-right").attr("scope", "row").text(key)
-        // head.addClass("text-right")
-        // head.attr("scope", "row")
-        // head.text(key)
-        row.append(head)
         var dataR = $("<td>").text(data[key])
-        // dataR.text(data[key])
-        row.append(dataR)
+        row.append(head).append(dataR)
         tbody.append(row)
     }
+
     table.append(tbody)
     tableDiv.append(table)
     dataDiv.append(tableDiv)
